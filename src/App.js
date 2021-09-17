5e82edac4dc70deeb7b281249d9eedde3f7efa3f
Sonu1507 <rsonu150794@gmail.com>
2021-09-17 23:15:57 +0530

    port and axios setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from "./services/request.config";
+import {useEffect} from "react";
 function App() {
+  useEffect(()=>{
+    axios.get("/").then(res=>console.log(res)).catch((err)=>console.log(err))
+  })
   return (
     <div className="App">
       <header className="App-header">
